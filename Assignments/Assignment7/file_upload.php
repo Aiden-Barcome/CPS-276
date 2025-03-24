c819b6d310ed6937d21fed8e2b27693e5c1b495e
Aiden Barcome <aidenbarcode@gmail.com>
2025-03-23 20:56:49 -0400

    updates

--- Assignments/Assignment7/file_upload.php
+++ Assignments/Assignment7/file_upload.php
@@ -1,16 +1,17 @@
-<?php
-$scott = "test";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>File Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <p>Hello </p>
+    <div class="mb-3">
+        <label for="fileName">File Name</label>
+        <input class="form-control" type="text" id="fileName">
+        <label for="fileUPload">File Upload</label>
+        <input class="form-control" type="file" id="fileUpload">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
